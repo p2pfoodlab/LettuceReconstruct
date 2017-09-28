4fe4c9de87aa0e54474c9cb21b9c87e80fd5ea3a
kodda <koddda@gmail.com>
2017-09-28 15:43:12 +0200

    few added...

--- python/pcd_utils.py
+++ python/pcd_utils.py
@@ -22,7 +22,6 @@ def getPC(filename):
    pc=np.hstack([np.array([ps[idx]["x"],ps[idx]["y"],ps[idx]["z"]]).T, ps[idx]["rgb"].view((np.uint8,4))[:,:3]])
    return pc
 
-
 def plot3d(pcds, colors=["k"], svg=None):
    fig = pl.figure()
    ax = fig.add_subplot(111, projection='3d')
@@ -38,3 +37,54 @@ def plot3d(pcds, colors=["k"], svg=None):
    ax.view_init(-80,50)
    if svg: pl.savefig(svg, bbox_inches="tight")
    else: pl.show()
+
+def rotation_from_matrix(matrix):
+    R = np.array(matrix, dtype=np.float64, copy=False)
+    R33 = R[:3, :3]
+    # direction: unit eigenvector of R33 corresponding to eigenvalue of 1
+    l, W = np.linalg.eig(R33.T)
+    i = np.where(abs(np.real(l) - 1.0) < 1e-5)[0]
+    if not len(i):
+        raise ValueError("no unit eigenvector corresponding to eigenvalue 1")
+    direction = np.real(W[:, i[-1]]).squeeze()
+    # point: unit eigenvector of R33 corresponding to eigenvalue of 1
+    l, Q = np.linalg.eig(R)
+    i = np.where(abs(np.real(l) - 1.0) < 1e-5)[0]
+    if not len(i):
+        raise ValueError("no unit eigenvector corresponding to eigenvalue 1")
+    point = np.real(Q[:, i[-1]]).squeeze()
+    point /= point[3]
+    # rotation angle depending on direction
+    cosa = (np.trace(R33) - 1.0) / 2.0
+    if abs(direction[2]) > 1e-5:
+        sina = (R[1, 0] + (cosa-1.0)*direction[0]*direction[1]) / direction[2]
+    elif abs(direction[1]) > 1e-5:
+        sina = (R[0, 2] + (cosa-1.0)*direction[0]*direction[2]) / direction[1]
+    else:
+        sina = (R[2, 1] + (cosa-1.0)*direction[1]*direction[2]) / direction[0]
+    angle = math.atan2(sina, cosa)
+    return angle, direction, point
+
+def get_hs(fname,N=100,s=1,saveit=False):
+   dirs=[]
+   angles=[]
+   ps=[]
+   Hs=[]
+   for k in range(s,N): 
+      H=np.fromfile(fname+'%s_%s.transform'%(k-s,k),dtype=np.float32).reshape([4,4])
+      angle, direction, point=rotation_from_matrix(H)
+      angles.append(angle)
+      dirs.append(direction)
+      ps.append(point)
+      Hs.append(H)
+   if saveit: np.save(saveit,H)
+   return np.array(angles), np.array(dirs), np.array(ps), np.array(Hs)
+
+def transf(tr,x):
+   return np.dot(tr[:3,:3],x.T).T+tr[:3,3]
+
+def invTransf(tr):
+   iTr=np.zeros([4,4])
+   iTr[:3,:3]=tr[:3,:3].T   
+   iTr[:3,3]=-np.dot(iTr[:3,:3],tr[:3,3])   
+   return iTr
--- python/test.py
+++ python/test.py
@@ -2,18 +2,30 @@ import pcd_utils as pu
 import numpy as np
 import matplotlib.pyplot as pl
 import sys
+from os import listdir
+from os.path import isfile, join
 
-data="planecut"
-filt="raw"
 
-pcd_folder="/home/kodda/Dropbox/p2pflab/LettuceScan/PCD_proc/data/csl_feuillue/%s"%data
-icp_folder="/home/kodda/Dropbox/p2pflab/LettuceScan/PCD_proc/data/csl_feuillue/%s/ICP_%sDS"%(data,filt)
+data="all"
+filt="filtered"
+
+#pcd_folder="/home/kodda/Dropbox/p2pflab/LettuceReconstruct/data/%s"%data
+pcd_folder="/home/kodda/Dropbox/p2pflab/LettuceReconstruct/data/%s"%data
+
+#ICP=np.loadtxt(join(pcd_folder,"ICPs_steps_0.txt") )
 
 i=0
-j=3
+j=(i+1)%120
 
-pcd1=pu.getPC(pcd_folder+"/"+filt+"/%03d.pcd"%i)
-pcd2=pu.getPC(pcd_folder+"/"+filt+"/%03d.pcd"%j)
-pcd3=pu.getPC(icp_folder+"/transform/%03d_%03d.pcd"%(i,j))
-pu.plot3d([pcd1,pcd2,pcd3], ["g","r","k"])
+source=pu.getPC(pcd_folder+"/"+filt+"/%03d.pcd"%i)[:,:3]
+target=pu.getPC(pcd_folder+"/"+filt+"/%03d.pcd"%j)[:,:3]
+s2t_pcl=pu.getPC(pcd_folder+"/transform/%03d_%03d.pcd"%(i,j))[:,:3]
 
+tr=np.fromfile(pcd_folder+'/transform/%03d_%03d.transform'%(i,j),dtype=np.float32).reshape([4,4])
+iTr=pu.invTransf(tr)
+s2t_py= pu.transf(tr, source)
+t2s_py= pu.transf(iTr, target)
+
+#pu.plot3d([source, target, s2t_py], ["g","r","k"])
+pu.plot3d([source, target, t2s_py], ["g","r","k"])
+#pu.plot3d([source, target, s2t_pcl], ["g","r","k"])
